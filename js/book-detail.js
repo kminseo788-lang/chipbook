aa1b8d77028e90b5a222a79a73bd6c3d6f2876e9
kminseo788-lang <kminseo788@gmail.com>
2026-05-20 23:59:27 +0900

    부제목필드추가

--- js/book-detail.js
+++ js/book-detail.js
@@ -77,6 +77,7 @@ if (isFree || purchased || isAuthor) {
       </div>
       <h1 class="book-detail-title">${book.title}</h1>
       <p class="book-detail-subtitle">${book.subtitle || ''}</p>
+<p class="book-detail-oneline">${book.one_line || ''}</p>
       <p class="book-detail-desc">${book.description || ''}</p>
       <div class="book-detail-meta">
      <a href="author-room.html?author_id=${book.author_id}" style="color:inherit">👤 ${authorName} 작가</a>
